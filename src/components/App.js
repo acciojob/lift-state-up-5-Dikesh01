2ca28480464e4f582d4c3f10a1b1bcc6b99c781a
kabir33559@gmail.com <Dikesh01>
2023-08-16 12:52:44 +0530

    Pyar tune kya kiya

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [user, setUser] = useState([])
+  const [success, setSuccess] = useState('')
+
+  function logginIn(){
+    if(user.length !== 0)
+    setSuccess('You are logged in!')
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+
+        {success && <p>{success}</p>}
+        
+           { success==='' &&
+             <div>
+              Username: <input type="text" onChange={(e)=>setUser([...user, {name:e.target.value}])}/>
+              <br />
+              <br />
+              Password: <input type="password" onChange={(e)=>setUser([...user, {password:e.target.value}])}/>
+              <br />
+              <br />
+              <button onClick={logginIn}>Log in</button>
+             </div>
+            }
+        
     </div>
   )
 }
